249babacbbccb73631673d7132a418787f412f5a
Voraton Lertrattanapaisal <voraton.l@ku.th>
2017-06-09 16:04:16 +0900

    Add Cursor

---
+++ Assets/Scripts/CursorPointer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorPointer : MonoBehaviour {
+    MeshRenderer renderer;
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponentInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hitTarget;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward,out hitTarget))
+        {
+            renderer.enabled = true;
+            transform.position = hitTarget.point;
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hitTarget.normal);
+        }else
+        {
+            renderer.enabled = false;
+        }
+	}
+}
